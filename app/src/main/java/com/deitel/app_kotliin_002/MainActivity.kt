4ec2bed247ada2d7b7464c5655bc969f1e071d41
ANAEKIA11 <98296971+ANAEKIA11@users.noreply.github.com>
2022-06-13 20:04:43 -0500

    act

--- app/src/main/java/com/deitel/app_kotliin_002/MainActivity.kt
+++ app/src/main/java/com/deitel/app_kotliin_002/MainActivity.kt
@@ -3,15 +3,19 @@ package com.deitel.app_kotliin_002
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.deitel.app_kotliin_002.databinding.ActivityMainBinding
+import com.deitel.app_kotliin_002.databinding.ActivitySegundaBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
+        /*
         val editTextUsuario = findViewById<EditText>(R.id.editTextUsuario);
         val editTextClave = findViewById<EditText>(R.id.editTextClave);
         val usuario = editTextUsuario.text.toString();
@@ -20,17 +24,28 @@ class MainActivity : AppCompatActivity() {
         val buttonIngresar = findViewById<Button>(R.id.buttonIngresar);
 
         buttonIngresar.setOnClickListener{
-            if((usuario.equals("admin")) && (clave.equals("admin"))){
+            if(usuario == "" && clave == ""){
                 val intent = Intent(this, SegundaActivity::class.java);
                 startActivity(intent);
             } else{
                 Toast.makeText(this, "Datos erroneos", Toast.LENGTH_LONG).show();
             }
         }
+        */
+
+        val binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+        val usuario = binding.editTextUsuario.text.toString()
+        val clave = binding.editTextClave.text.toString()
 
 
+        binding.buttonIngresar.setOnClickListener {
+            /*if(usuario == "admin" && clave == "admin"){
 
-
+            }else{
+                Toast.makeText(this, "Datos erroneos", Toast.LENGTH_LONG).show();
+            }*/
+        }
 
     }
 }
--- app/src/main/java/com/deitel/app_kotliin_002/SegundaActivity.kt
+++ app/src/main/java/com/deitel/app_kotliin_002/SegundaActivity.kt
@@ -2,13 +2,27 @@ package com.deitel.app_kotliin_002
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.deitel.app_kotliin_002.databinding.ActivitySegundaBinding
 
 class SegundaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_segunda)
+        //setContentView(R.layout.activity_segunda)
 
-        val binding = ActivityM
+        val binding = ActivitySegundaBinding.inflate(layoutInflater);
+        setContentView(binding.root);
 
+        binding.buttonMostrar.setOnClickListener {
+            val mensaje = binding.editTextMensaje.text.toString()
+
+            Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+        }
+
+        binding.buttonSalir.setOnClickListener {
+            finish()
+            //val texto = "FISEI";
+            //binding.editTextMensaje.setText(texto);
+        }
     }
 }
